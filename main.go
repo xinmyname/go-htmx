03c842f6d332954e29f27b923fdacd7082cf7257
Andy Sherwood <xinmyname@gmail.com>
2024-02-23 20:40:28 -0800

    serve static files

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
 	"time"
 )
 
@@ -16,7 +17,14 @@ type Film struct {
 func main() {
 	fmt.Println("go-htmx started! ", time.Now().Format("2006-01-02 15:04:05"))
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(safeStaticFileSystem{http.Dir("./static")})
+
+	mux.Handle("/static", http.NotFoundHandler())
+	mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		tmpl := template.Must(template.ParseFiles("index.html"))
 
 		films := map[string][]Film{
@@ -33,7 +41,7 @@ func main() {
 		tmpl.Execute(w, films)
 	})
 
-	http.HandleFunc("/add-film/", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/add-film/", func(w http.ResponseWriter, r *http.Request) {
 		title := r.PostFormValue("title")
 		director := r.PostFormValue("director")
 
@@ -43,5 +51,31 @@ func main() {
 		tmpl.ExecuteTemplate(w, "film-list-element", Film{Title: title, Director: director})
 	})
 
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	log.Fatal(http.ListenAndServe(":8000", mux))
+}
+
+type safeStaticFileSystem struct {
+	fs http.FileSystem
+}
+
+func (ssfs safeStaticFileSystem) Open(path string) (http.File, error) {
+	f, err := ssfs.fs.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	s, _ := f.Stat()
+	if s.IsDir() {
+		index := filepath.Join(path, "index.html")
+		if _, err := ssfs.fs.Open(index); err != nil {
+			closeErr := f.Close()
+			if closeErr != nil {
+				return nil, closeErr
+			}
+
+			return nil, err
+		}
+	}
+
+	return f, nil
 }

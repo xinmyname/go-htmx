404c38b385aceafb573c2075e047eaacc53ca610
Andy Sherwood <xinmyname@gmail.com>
2024-02-23 17:21:41 -0800

    Just added HTMX

---
+++ README.md
@@ -0,0 +1,5 @@
+Go HTMX Tutorial
+================
+This is my build of BugBytes golang + HTMX tutorial available here: 
+https://www.youtube.com/watch?v=F9H6vYelYyU
+
--- main.go
+++ main.go
@@ -7,12 +7,34 @@ import (
 	"net/http"
 )
 
+type Film struct {
+	Title    string
+	Director string
+}
+
 func main() {
 	fmt.Println("Hello, World!")
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		tmpl := template.Must(template.ParseFiles("index.html"))
-		tmpl.Execute(w, nil)
+
+		films := map[string][]Film{
+			"Films": {
+				{"Everything Everywhere All at Once", "Daniel Kwan, Daniel Scheinert"},
+				{"The Matrix", "Lana Wachowski, Lilly Wachowski"},
+				{"The Shawshank Redemption", "Frank Darabont"},
+				{"Wonder Woman", "Patty Jenkins"},
+				{"Interstellar", "Christopher Nolan"},
+				{"Anatomy of a Fall", "Justine Triet"},
+			},
+		}
+
+		tmpl.Execute(w, films)
+	})
+
+	http.HandleFunc("/add-film/", func(w http.ResponseWriter, r *http.Request) {
+		log.Print("HTMLX request received")
+		log.Print(r.Header.Get("HX-Request"))
 	})
 
 	log.Fatal(http.ListenAndServe(":8000", nil))
